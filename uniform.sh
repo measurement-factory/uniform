2b29afb966f5ca83ff52ffa7c49ad2e741b522e6
Mark-Simulacrum <mark.simulacrum@gmail.com>
2015-06-09 17:08:38 -0600

    Initial commit of uniform.sh. Currently only supports javascript file linting.

---
+++ uniform.sh
@@ -0,0 +1,46 @@
+#! /usr/bin/env sh
+
+use_force=""
+if test "$1" = "--force"; then
+    use_force=1
+    shift
+fi
+
+if test "$1" = "--"; then
+    shift
+fi
+
+args=$@
+
+# --name-only = Only show filepath
+# -r = Recurse into subtrees
+# HEAD = look at current git commit
+# $args is passed to allow specifying specific files
+files=`git ls-tree -r --name-only HEAD $args`
+
+if !(git diff --name-only --exit-code $args); then
+    echo "The files listed above have been changed since last commit."
+
+    if test -z "$use_force"; then
+        echo "Commit these files or run with --force."
+        exit 1
+    else
+        echo "Proceeding anyway: --force given."
+    fi
+fi
+
+for file in $files; do
+    # run formatter on file
+    filename=$(basename $file)
+    extension=${filename##*.}
+
+    if test $extension = "js"; then
+        tempfile=`mktemp`
+        node ./lib/index.js $file 1>$tempfile
+        exit_code=$?
+        if test $exit_code -ne 0; then
+            exit $exit_code
+        fi
+        mv $tempfile $file
+    fi
+done
